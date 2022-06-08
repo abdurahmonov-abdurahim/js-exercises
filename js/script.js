793cfdd9e810175d651b688b2bddf0d696e3aae1
abdurahmonov-abdurahim <foniyrahim@gmail.com>
2022-06-08 08:40:47 +0500

    exercise

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// 1-exercise
+
+// const userValue = Number(prompt("Raqam kiriting"));
+
+// const calcValueList = [2, 7]
+
+// const sumInput = calcValue => {
+
+//   calcValueList.push(calcValue);
+
+//   if (isNaN(calcValue) && (calcValue == " ") && (calcValue == "")) {
+//     console.log("Brat raqam kiriting");
+//   } else {
+//     const initialValue = 0;
+//     const sumWithInitial = calcValueList.reduce(
+//       (previousValue, calcValue) => previousValue + calcValue,
+//       initialValue);
+//     console.log(sumWithInitial);
+//   }
+// }
+
+// console.log(sumInput(userValue));
+
+
+// 2-Exercise
+
+// const musicalDirection = ['Jazz', 'Blues'];
+
+// musicalDirection.push('Rock-n-rol')
+
+// console.log(musicalDirection);
+
+// console.log(musicalDirection.shift());
+
+// musicalDirection.unshift('Rap', 'Reggae')
+
+// console.log(musicalDirection);
+
+
+
+// EXERCISE 3
+
+
+// let salaries = {
+//   "Akmal": 1200,
+//   "Salim": 5200,
+//   "Karima": 1800
+// }
+
+// const objectArray = Object.values(salaries);
+
+// const sumSalaries = obj => {
+
+//   const initialValue = 0;
+
+//   const sumObject = obj.reduce(
+//     (previousValue, currentValue) => previousValue + currentValue,
+//     initialValue
+//   );
+
+
+
+//   console.log(sumObject);
+// }
+
+// sumSalaries(objectArray)
+
+// EXERCISE 4
